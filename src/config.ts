ad68f52aa3c6776422027a1f2add9608f3635d9a
yichu <50491873+d22818977@users.noreply.github.com>
2026-08-17 21:43:53 +0800

    feat: add Warsaw memoir entries

--- src/config.ts
+++ src/config.ts
@@ -201,3 +201,4 @@ export const defaultLocale = themeConfig.global.locale
 export const moreLocales = themeConfig.global.moreLocales
 export const allLocales = [defaultLocale, ...moreLocales]
 export const heroBackgroundVideoURL = 'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260405_171521_25968ba2-b594-4b32-aab7-f6b69398a6fa.mp4'
+export const heroBackgroundImageURL = `${base}/images/site-background/monumental-landscape.png`
